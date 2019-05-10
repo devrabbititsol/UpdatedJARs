cf53a5a8c47ee53dcb59a9722a9a2a2acf097d2d
root <santhosh.annam@devrabbit.com>
2019-05-10 16:11:04 +0530

    addingAPI

---
+++ src/test/java/api/updatedjars/googleapi/apipageclasses/GoogleAPIGet2Dataset.java
@@ -0,0 +1,84 @@
+package api.updatedjars.googleapi.apipageclasses; 
+
+public class GoogleAPIGet2Dataset{
+
+public static final int datasetLength = 1; 
+
+public static String projectName = "updatedjars";
+
+ public static String primaryInfo  = "{\"module_description\":\"desc\","
+ + "\"mobile_platform\":null,"
+ + "\"report_upload_url\":\"https://192.168.1.142:8080/UploadReportFile\","
+ + "\"repository_commit_message\":\"addingAPI\","
+ + "\"browser_type\":null,"
+ + "\"project_url\":\"https://maps.googleapis.com/\","
+ + "\"jenkins_username\":\"admin\","
+ + "\"testcase_id\":252,"
+ + "\"project_name\":\"UpdatedJARs\","
+ + "\"testset_name\":null,"
+ + "\"is_web\":false,"
+ + "\"jenkins_password\":\"admin\","
+ + "\"project_id\":290,"
+ + "\"framework_path\":\"/home/devrabbit/SmartQE/SmartQE-Users/7/Selenium/UpdatedJARs\","
+ + "\"sub_module_id\":0,"
+ + "\"repository_url\":\"Vvl8DW1e8+K7CCRYoPUdQbs6APB5pHVVEC284LpOJaFTcjmo4xY6kA0OZ/VnVlxiA1xC1klvgxuYRi2lz2aXs7mfhUQgpzWBiO9LdeHiwwR7avXt8Kt/D8iiqY2EqcrX\","
+ + "\"sub_module_description\":null,"
+ + "\"testcase_name\":\"TC2\","
+ + "\"executed_timestamp\":1557484861248,"
+ + "\"is_execute\":false,"
+ + "\"file_name\":null,"
+ + "\"aes_secret_key\":\"_SmartQE_SmartQE\","
+ + "\"testset_id\":0,"
+ + "\"jenkins_token\":\"11b2bb6ad5a0829d41a3149cea7bf2b07f\","
+ + "\"is_generate\":false,"
+ + "\"jenkins_url\":\"http://192.168.1.142:8081/\","
+ + "\"project_description\":\"desc\","
+ + "\"module_id\":395,"
+ + "\"user_id\":7,"
+ + "\"bundle_id\":null,"
+ + "\"sub_module_name\":null,"
+ + "\"device_os_version\":null,"
+ + "\"execution_environment\":\"jenkins\","
+ + "\"module_name\":\"GoogleAPI\"}";
+
+
+			/*--------Default Dataset-----------*/
+
+public static final String datasetHeader1 = "TC2-Default";
+
+public static final String datasetResources1 = "maps/api/place/nearbysearch/json";
+public static final int requestType1 = 1;
+public static final int bodyType1 = -1;
+
+ public static String urlParams1  = "[{\"param_value\":\"-33.8670522,"
+ + "151.1957362\","
+ + "\"url_param_id\":848,"
+ + "\"param_key\":\"location\","
+ + "\"dataset_id\":1250,"
+ + "\"param_desc\":\"\"},"
+ + "{\"param_value\":\"restaurant\","
+ + "\"url_param_id\":849,"
+ + "\"param_key\":\"type\","
+ + "\"dataset_id\":1250,"
+ + "\"param_desc\":\"\"},"
+ + "{\"param_value\":\"cruise\","
+ + "\"url_param_id\":850,"
+ + "\"param_key\":\"keyword\","
+ + "\"dataset_id\":1250,"
+ + "\"param_desc\":\"\"},"
+ + "{\"param_value\":\"AIzaSyBhALFz5ldUASRKUXEhv1iXjXKE8OlULyk\","
+ + "\"url_param_id\":851,"
+ + "\"param_key\":\"key\","
+ + "\"dataset_id\":1250,"
+ + "\"param_desc\":\"\"},"
+ + "{\"param_value\":\"2500\","
+ + "\"url_param_id\":852,"
+ + "\"param_key\":\"radius\","
+ + "\"dataset_id\":1250,"
+ + "\"param_desc\":\"\"}]";
+
+ public static String headers1  = "[]";
+
+ public static String body1  = "{}";
+
+}
---
+++ src/test/java/api/updatedjars/googleapi/apitestclasses/TC2.java
@@ -0,0 +1,83 @@
+package api.updatedjars.googleapi.apitestclasses;
+
+import com.restassured.services.*;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;import com.utilities.BaseClass;
+import com.utilities.ConfigFilesUtility;
+import com.configurations.*;
+import api.updatedjars.googleapi.apipageclasses.GoogleAPIGet2Dataset;
+import api.updatedjars.googleapi.apipageclasses.GoogleAPIGetDataset;
+public class TC2 extends BaseClass {
+	ExtentReports reports;
+	ExtentTest test;
+	ITestResult result;
+	private Logger logger;
+	private ConfigFilesUtility configFileObj;
+	String projectPath = System.getProperty("user.dir");
+	String reportsPath = projectPath + "/APIReports/" + ReportPaths.reportPathName;
+
+	public TC2() throws Exception {
+		PropertyConfigurator.configure("log4j.properties");
+		logger = Logger.getLogger(TC2.class);
+		configFileObj= new ConfigFilesUtility();
+		configFileObj.loadPropertyFile("apiconfig.properties");
+	}
+
+	@Test(priority = 1)
+	public void doRequest() throws Exception { 
+
+		String primaryInfo = GoogleAPIGetDataset.primaryInfo;
+		Constants.PRIMARY_INFO = primaryInfo;
+		reports = ExtentConfigurations.getExtentInstance(reportsPath, projectPath ,GoogleAPIGetDataset.projectName);
+		test = reports.startTest("Verifying TC2 API");
+		int datasetLength = GoogleAPIGetDataset.datasetLength; 
+		for(int i = 1; i <= datasetLength; i++) {
+
+			boolean isExecutionFlag = true;			
+		/*--------------------- GET --------------------- */
+
+			if(isExecutionFlag) {
+ 			 	Object object = (Object) new GoogleAPIGet2Dataset();
+				Class<?> c = object.getClass();
+				String urlParams = (String) c.getField("urlParams" + i).get(object);
+				if(urlParams == null) return;
+				String headers = (String) c.getField("headers" + i).get(object);
+				String body = (String) c.getField("body" + i).get(object);
+				String datasetHeader = (String) c.getField("datasetHeader" + i).get(object);
+				String datasetResources = (String) c.getField("datasetResources" + i).get(object);
+				int reqType = (Integer) c.getField("requestType" + i).get(object);
+				int bodyType = (Integer) c.getField("bodyType" + i).get(object);
+			 	Constants.testName = Constants.testName + datasetHeader;
+				String response = APIService.callRequest(primaryInfo, urlParams, headers, reqType, bodyType, body, datasetHeader, datasetResources, test, logger);
+			}
+
+		/*--------------------- GET --------------------- */
+
+			if(isExecutionFlag) {
+ 			 	Object object = (Object) new GoogleAPIGetDataset();
+				Class<?> c = object.getClass();
+				String urlParams = (String) c.getField("urlParams" + i).get(object);
+				if(urlParams == null) return;
+				String headers = (String) c.getField("headers" + i).get(object);
+				String body = (String) c.getField("body" + i).get(object);
+				String datasetHeader = (String) c.getField("datasetHeader" + i).get(object);
+				String datasetResources = (String) c.getField("datasetResources" + i).get(object);
+				int reqType = (Integer) c.getField("requestType" + i).get(object);
+				int bodyType = (Integer) c.getField("bodyType" + i).get(object);
+			 	Constants.testName = Constants.testName + datasetHeader;
+				String response = APIService.callRequest(primaryInfo, urlParams, headers, reqType, bodyType, body, datasetHeader, datasetResources, test, logger);
+			}
+		}
+	}
+
+	@AfterClass
+	public void tearDown() throws Exception {
+		reports.endTest(test);
+		reports.flush();
+	}
+}
